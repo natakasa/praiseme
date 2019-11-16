b19ad31426c88779f3ada300262d9991b69e14e0
natakasa <natakasa@example.com>
2019-11-16 21:12:39 +0900

    vue導入

--- app/javascript/top.js
+++ app/javascript/top.js
@@ -1,3 +1,39 @@
-$('.infomation').click(function() {
+import Vue from 'vue/dist/vue.esm';
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    var app = new Vue({
+        el: ".infomation",
+        data: {
+            show: false
+        },
+        methods: {
+            infoSwich: function () {
+                this.show = !this.show
+            },
+            infoClose: function () {
+                this.show = false
+            }
+        }
+    });
+
+    var app = new Vue({
+        el: ".char_select",
+        data: {
+            isOpen: false
+        },
+        methods: {
+            charSwich: function () {
+                this.isOpen = !this.isOpen
+            }
+        }
+    });
+
+});
+
+/* jquery
+$(document).on('click', '.infomation', function() {
+    alert( "infomation" )
     $('.infomation_contents').show();
 });
+*/
