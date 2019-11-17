f359fc63b636c742d0f9ad42fec88f1b90da2c21
natakasa <natakasa@example.com>
2019-11-17 18:27:42 +0900

    不要なコメント削除

--- app/controllers/twitt_controller.rb
+++ app/controllers/twitt_controller.rb
@@ -83,46 +83,4 @@ class TwittController < ApplicationController
     redirect_to root_path, notice: "success!"
     image.destroy!
   end
-  
-
-  __END__
-   # ライブラリ化のため廃止
-   # セリフを形成する
-  def make_line(base_line)
-  
-    base_line0 = 'すごいやん！'
-    base_line1 = '@dなんて、誰しもができるもんやないで。'
-    base_line2 = 'しかも@mかぁ…ほんますごいわ。'
-    base_line3 = '無理せんと、しっかり気分転換するんやで！'
-  
-    natto = Natto::MeCab.new
-
-    m_flg = false
-    d_flg = false
-
-    natto.parse(base_line) do |n|
-    
-      if n.feature.start_with?('動詞') then
-        base_line1.sub!('@d', n.surface)
-        d_flg = true
-      end
-
-      if n.feature.start_with?('名詞') then
-        base_line2.sub!('@m', n.surface)
-        m_flg = true
-      end
-    end
-    
-    # 文字列の連結
-    final_line = base_line0
-    if d_flg then
-      final_line = final_line + base_line1
-    end
-    if m_flg then
-      final_line = final_line + base_line2
-    end
-    final_line = final_line + base_line3
-    
-    return final_line
-  end
 end
